7e4607619bb835a42719cb550a30fac666741b55
rasidb <rasid5888@gmail.com>
2023-12-05 03:18:37 +0300

    pom.xml testNG bağımlılığı eklendi TryConnectToDB.java eklendi

---
+++ src/test/java/com/sql/TryConnectToDB.java
@@ -0,0 +1,19 @@
+package com.sql;
+
+import com.utilities.DBUtils;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TryConnectToDB {
+    @Test
+    public void connectingDB() {
+        DBUtils.createConnection();
+        List<List<Object>> queryResultList = DBUtils.getQueryResultList("select * from employees");
+        for (List<Object> objects : queryResultList) {
+            System.out.println(objects);
+        }
+        DBUtils.destroy();
+
+    }
+}
