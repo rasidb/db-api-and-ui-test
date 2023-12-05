65fb7b4d75b1eeba8af32ff40af5ca40e67a2a78
rasidb <rasid5888@gmail.com>
2023-12-06 00:55:58 +0300

    SQL_Notes1.java having_avg methodu eklendi

--- src/test/java/com/sqlNotes/SQL_Notes1.java
+++ src/test/java/com/sqlNotes/SQL_Notes1.java
@@ -4,6 +4,7 @@ import com.utilities.DBUtils;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.Map;
 
 public class SQL_Notes1 {
     /*
@@ -26,6 +27,28 @@ public class SQL_Notes1 {
         // Sonuçları ekrana yazdır
         for (List<Object> objects : queryResultList) {
             System.out.println(objects);
+        }
+
+        // Veritabanı bağlantısını kapat
+        DBUtils.destroy();
+    }
+    @Test
+    public void having_avg(){
+        // veritabanına bağlan
+        DBUtils.createConnection();
+
+        //sql sorgusunu oluştur (birbirinden farklı pozisyonlar bu pozisyonlarda çalışan kişi sayıları ve ortalama maaşları arasından min 10000 kazananlar)
+        String sql = "select job_id, avg(salary), count(*) ,sum(salary)" +
+                " from employees" +
+                " group by job_id" +
+                " having avg(salary)>10000";
+
+        //sql sorgusunu yürüt ve sonuçları list olarak al
+        List<List<Object>> queryResultList = DBUtils.getQueryResultList(sql);
+
+        //sonuçları ekrana yazdır
+        for (List<Object> stringObjectMap : queryResultList) {
+            System.out.println(stringObjectMap);
         }
 
         // Veritabanı bağlantısını kapat
