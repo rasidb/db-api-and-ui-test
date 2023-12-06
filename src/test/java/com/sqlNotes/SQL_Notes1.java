e28d7973e6c86bab437eca22e23e7e6e0b6a226e
rasidb <rasid5888@gmail.com>
2023-12-06 03:24:50 +0300

    SQL_Notes1.java practice5 methodu eklendi

--- src/test/java/com/sqlNotes/SQL_Notes1.java
+++ src/test/java/com/sqlNotes/SQL_Notes1.java
@@ -8,18 +8,21 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 public class SQL_Notes1 {
     /*
-     GROUP BY
-     GROUP BY ifadesi, SELECT ifadesinden alınan satırları belirttiğimiz "job_id" alanına göre gruplara ayırır.
- */
+         GROUP BY
+         GROUP BY ifadesi, SELECT ifadesinden alınan satırları belirttiğimiz "job_id" alanına göre gruplara ayırır.
+     */
     @Test
-    public void group_by(){
+    public void group_by() {
         // Veritabanına bağlan
         DBUtils.createConnection();
 
         // SQL sorgusunu oluştur
-        String sql="SELECT job_id, avg(salary) " +
+        String sql = "SELECT job_id, avg(salary) " +
                 "FROM employees " +
                 "GROUP BY job_id";
 
@@ -34,8 +37,9 @@ public class SQL_Notes1 {
         // Veritabanı bağlantısını kapat
         DBUtils.destroy();
     }
+
     @Test
-    public void having_avg(){
+    public void having_avg() {
         // veritabanına bağlan
         DBUtils.createConnection();
 
@@ -58,14 +62,14 @@ public class SQL_Notes1 {
     }
 
     @Test
-    public void distinct(){
+    public void distinct() {
         //birbirinden farklı job_id'leri getir
 
         //db bağlantısı kur
         DBUtils.createConnection();
 
         //sql sorgusunu oluştur
-        String sql ="select distinct job_id " +
+        String sql = "select distinct job_id " +
                 "from employees";
 
         //sorgunun sonucunu liste olarak al
@@ -79,17 +83,18 @@ public class SQL_Notes1 {
             DBUtils.destroy();
         }
 
-        Assert.assertTrue(queryResultList.size()!=0);
+        assertTrue(queryResultList.size() != 0);
     }
+
     @Test
-    public void practice(){
+    public void practice() {
         //get me all info who is working as IT _PROG or SA_REP
 
         //db bağlantısı kur
         DBUtils.createConnection();
 
         //sql sorgusunu oluştur
-        String sql ="select * " +
+        String sql = "select * " +
                 "from employees " +
                 "where job_id in ('IT_PROG', 'SA_REP')";
 
@@ -98,19 +103,20 @@ public class SQL_Notes1 {
 
         for (List<Object> users : queryResultList) {
             //job_id'nin it_prog ya da sa_rep olduğunu doğrula
-            Assert.assertTrue(users.get(6).equals("IT_PROG") || users.get(6).equals("SA_REP"));
+            assertTrue(users.get(6).equals("IT_PROG") || users.get(6).equals("SA_REP"));
         }
 
         //db bağlantısından çık
         DBUtils.destroy();
     }
+
     @Test
-    public void practice2(){
+    public void practice2() {
         //how many employees making more than 8000
         //db bağlantısını yap
         DBUtils.createConnection();
         // sql sorgusunu hazırla
-        String sql ="select * " +
+        String sql = "select * " +
                 "from employees " +
                 "where salary >8000";
         //sql sorgusunu gönder ve bir map olarak depola
@@ -119,14 +125,86 @@ public class SQL_Notes1 {
         for (Map<String, Object> objects : queryResultMap) {
             //8000den fazla olduğunu doğrula
             BigDecimal salary = (BigDecimal) objects.get("SALARY");
-            Assert.assertTrue(salary.compareTo(new BigDecimal(8000))>0);
+            assertTrue(salary.doubleValue() > 8000);
         }
         DBUtils.destroy();
     }
 
     @Test
-    public void practice3(){
+    public void practice3() {
         //en fazla maaş alandan en az maaş alana kadar sırala
 
+        //db bağlantısını yap
+        DBUtils.createConnection();
+
+        //sql sorgusu oluştur
+        String sql = "select * " +
+                "from employees " +
+                "order by salary desc ;";
+
+        //sorguyu gönder ve cevabı map olarak al
+        List<Map<String, Object>> queryResultMap = DBUtils.getQueryResultMap(sql);
+
+        //maaşların çoktan aza doğru sıralandığını doğrula
+        BigDecimal beforeSalary = BigDecimal.valueOf(0);
+        for (Map<String, Object> user : queryResultMap) {
+            BigDecimal salary = (BigDecimal) user.get("SALARY");
+            assertTrue(salary.doubleValue() >= beforeSalary.doubleValue());
+            beforeSalary = salary;
+        }
+        DBUtils.destroy();
     }
+
+    @Test
+    public void practice4() {
+        //en fazla maaş alan ilk 10 kişiyi sırala (duplicate olmadan)
+
+        //bağlantıyı kur
+        DBUtils.createConnection();
+
+        //sql sorgusu oluştur
+        String sql = "select * " +
+                "from(select * from employees order by salary desc)" +
+                "where rownum <11";
+
+        //sorguyu gönder ve sonucu kaydet
+        List<Map<String, Object>> queryResultMap = DBUtils.getQueryResultMap(sql);
+        for (Map<String, Object> o : queryResultMap) {
+            System.out.println(o);
+        }
+
+        //db bağlantısını kapat
+        DBUtils.destroy();
+    }
+
+    @Test
+    public void practice5(){
+        //-get me 5 letter first names where the middle char is z;
+
+        //create db connection
+        DBUtils.createConnection();
+
+        //create sql request
+        String sql ="select * " +
+                "from employees " +
+                "where first_name like '__z__'";
+
+        /*
+        String sql = "SELECT * " +
+             "FROM employees " +
+             "WHERE LENGTH(first_name) = 5 AND first_name LIKE '__z__'";
+
+         */
+
+        //send sql request and save the response as a list of map
+        List<Map<String, Object>> queryResultMap = DBUtils.getQueryResultMap(sql);
+
+        //assert it third letter is 'z' for all first_names
+        for (Map<String, Object> user : queryResultMap) {
+            String name = (String) user.get("FIRST_NAME");
+           assertEquals(name.charAt(2),'z');
+        }
+        DBUtils.destroy();
+    }
+
 }
