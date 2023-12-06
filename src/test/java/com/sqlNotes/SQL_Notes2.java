1bf5e8c7c6ef82a6d9b6fb93243eee2090bd851e
rasidb <rasid5888@gmail.com>
2023-12-06 03:52:10 +0300

    SQL_Notes2.java length keyword örneği eklendi

--- src/test/java/com/sqlNotes/SQL_Notes2.java
+++ src/test/java/com/sqlNotes/SQL_Notes2.java
@@ -25,4 +25,21 @@ public class SQL_Notes2 {
         }
         DBUtils.destroy();
     }
+    @Test
+    public void length(){
+        /* first_name ve last_name nesnelerini birleştirerek fullName ve fullName nesnesinin uzunluğunun olduğu tablo hazırla
+        ve  fullname uzunluğunu çoktan aza doğru sırala*/
+        DBUtils.createConnection();
+
+        String sql="select first_name || ' ' || last_name as fullName, LENGTH(first_name || last_name) + 1 as length " +
+                "from employees " +
+                "order by length desc";
+
+        List<Map<String, Object>> queryResultMap = DBUtils.getQueryResultMap(sql);
+
+        for (Map<String, Object> stringObjectMap : queryResultMap) {
+            Assert.assertFalse(stringObjectMap.get("LENGTH").equals(0));
+        }
+        DBUtils.destroy();
+    }
 }
