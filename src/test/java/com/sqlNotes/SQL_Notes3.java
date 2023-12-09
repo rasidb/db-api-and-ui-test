0fa327a6908dd790af0a821838e47ff0063b7657
rasidb <rasid5888@gmail.com>
2023-12-09 16:32:22 +0300

    SQL_Notes3.java DDL DML DERS NOTU EKLENDİ

--- src/test/java/com/sqlNotes/SQL_Notes3.java
+++ src/test/java/com/sqlNotes/SQL_Notes3.java
@@ -1,4 +1,23 @@
 package com.sqlNotes;
 
+import com.utilities.DBUtils;
+import org.testng.annotations.Test;
+
 public class SQL_Notes3 {
+    /*
+_____________________________________________________________________________________________________
+|    DDL: data definition language                 |           DML: data manipulation language       |
+|    it is used to define data structures          |         it is used to manipulate data itself    |
+|    *CREATE                                       |          *SELECT                                |
+|    *DROP                                         |          *INSERT                                |
+|    *TRUNCATE                                     |          *UPDATE                                |
+|    *ALTER                                        |          *DELETE                                |
+______________________________________________________________________________________________________
+
+     */
+
+    @Test
+    void DDL() {
+
+    }
 }
